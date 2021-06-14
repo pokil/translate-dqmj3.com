0fcc467ee755462f1e337e4ed5c392541a6b9a67
pokil <cradfire@gmail.com>
2021-06-14 22:23:47 +0700

    Create dqmj3.com.meta.js

---
+++ dqmj3.com.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        translate dqmj3.com
+// @namespace   translate-dqmj3.com
+// @match       https://dqmj3.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description translate monster name dqmj3.com
+// @updateURL 	https://github.com/pokil/translate-dqmj3.com/raw/master/dqmj3.com.meta.js
+// @downloadURL https://github.com/pokil/translate-dqmj3.com/raw/master/dqmj3.com.user.js
+// ==/UserScript==
