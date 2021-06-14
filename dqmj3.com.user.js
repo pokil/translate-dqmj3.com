fd82440521046c4bdbe5369d1c786a31d93d4967
pokil <cradfire@gmail.com>
2021-06-14 22:25:17 +0700

    Update dqmj3.com.user.js

--- dqmj3.com.user.js
+++ dqmj3.com.user.js
@@ -6,8 +6,8 @@
 // @version     1.0
 // @author      -
 // @description translate monster name dqmj3.com
-// @updateURL 	https://github.com/pokil/translate-dqmj3.com/raw/master/dqmj3.com.meta.js
-// @downloadURL https://github.com/pokil/translate-dqmj3.com/raw/master/dqmj3.com.user.js
+// @updateURL 	https://github.com/pokil/translate-dqmj3.com/raw/main/dqmj3.com.meta.js
+// @downloadURL https://github.com/pokil/translate-dqmj3.com/raw/main/dqmj3.com.user.js
 // ==/UserScript==
 
 
